353183bc740b48cc3cfc011f8be7d32d8088bfed
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2015-09-12 13:22:29 -0500

    Removed unused About menu from GUI and refactored methods relating to it.

--- src/colorpicker/ColorPicker.java
+++ src/colorpicker/ColorPicker.java
@@ -53,10 +53,6 @@ public class ColorPicker extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        aboutFrame = new javax.swing.JFrame();
-        aboutContainer = new javax.swing.JPanel();
-        aboutLabel = new javax.swing.JLabel();
-        aboutExitButton = new javax.swing.JButton();
         settingsPanel = new javax.swing.JPanel();
         redLabel = new javax.swing.JLabel();
         greenLabel = new javax.swing.JLabel();
@@ -71,66 +67,6 @@ public class ColorPicker extends javax.swing.JFrame {
         menuBarFile_New = new javax.swing.JMenuItem();
         menuBarFile_Separator = new javax.swing.JPopupMenu.Separator();
         menuBarFile_Exit = new javax.swing.JMenuItem();
-        menuBarHelp = new javax.swing.JMenu();
-        menuBarHelp_About = new javax.swing.JMenuItem();
-
-        aboutFrame.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        aboutFrame.setTitle("About");
-        aboutFrame.setAlwaysOnTop(true);
-        aboutFrame.setLocationByPlatform(true);
-        aboutFrame.setResizable(false);
-
-        aboutContainer.setBorder(javax.swing.BorderFactory.createTitledBorder("About This Program"));
-        aboutContainer.setToolTipText("");
-
-        aboutLabel.setText("<html><p>This program was written by Oliver Abdulrahim for AP Computer Science class.<br><br>To report a bug, email <u>n.2147483648@gmail.com</u> with steps to reproduce it.</p></html>");
-
-        aboutExitButton.setText("OK");
-        aboutExitButton.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseReleased(java.awt.event.MouseEvent evt) {
-                aboutExitButtonMouseReleased(evt);
-            }
-        });
-
-        javax.swing.GroupLayout aboutContainerLayout = new javax.swing.GroupLayout(aboutContainer);
-        aboutContainer.setLayout(aboutContainerLayout);
-        aboutContainerLayout.setHorizontalGroup(
-            aboutContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(aboutContainerLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(aboutLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                .addContainerGap())
-            .addGroup(aboutContainerLayout.createSequentialGroup()
-                .addGap(106, 106, 106)
-                .addComponent(aboutExitButton)
-                .addContainerGap(111, Short.MAX_VALUE))
-        );
-        aboutContainerLayout.setVerticalGroup(
-            aboutContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(aboutContainerLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(aboutLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(aboutExitButton)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        javax.swing.GroupLayout aboutFrameLayout = new javax.swing.GroupLayout(aboutFrame.getContentPane());
-        aboutFrame.getContentPane().setLayout(aboutFrameLayout);
-        aboutFrameLayout.setHorizontalGroup(
-            aboutFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(aboutFrameLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(aboutContainer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        aboutFrameLayout.setVerticalGroup(
-            aboutFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(aboutFrameLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(aboutContainer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
-        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -252,18 +188,6 @@ public class ColorPicker extends javax.swing.JFrame {
 
         menuBar.add(menuBarFile);
 
-        menuBarHelp.setText("Help");
-
-        menuBarHelp_About.setText("About");
-        menuBarHelp_About.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menuBarHelp_AboutActionPerformed(evt);
-            }
-        });
-        menuBarHelp.add(menuBarHelp_About);
-
-        menuBar.add(menuBarHelp);
-
         setJMenuBar(menuBar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -296,20 +220,6 @@ public class ColorPicker extends javax.swing.JFrame {
     private void menuBarFile_ExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuBarFile_ExitActionPerformed
         System.exit(0);
     }//GEN-LAST:event_menuBarFile_ExitActionPerformed
-
-    /**
-     * Displays an informative window about this program.
-     */
-    private void menuBarHelp_AboutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuBarHelp_AboutActionPerformed
-        showAboutFrame();
-    }//GEN-LAST:event_menuBarHelp_AboutActionPerformed
-
-    /**
-     * Exits the informative program window.
-     */
-    private void aboutExitButtonMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_aboutExitButtonMouseReleased
-        aboutFrame.dispose();
-    }//GEN-LAST:event_aboutExitButtonMouseReleased
 
     /**
      * Updates the red component of {@link #colorLabel}.
@@ -363,16 +273,6 @@ public class ColorPicker extends javax.swing.JFrame {
     }
     
     /**
-     * Displays an informative window about this program.
-     */
-    private void showAboutFrame() {
-        // Should let the operating system decide where to put the window, but
-        // it defaults to the center on Windows, which can be jarring.
-        aboutFrame.setBounds(getX() + 10, getY() + 10, 300, 200);
-        aboutFrame.setVisible(true);
-    }
-    
-    /**
      * Creates and displays an instance of this class.
      * 
      * @param args The command-line arguments.
@@ -400,10 +300,6 @@ public class ColorPicker extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel aboutContainer;
-    private javax.swing.JButton aboutExitButton;
-    private javax.swing.JFrame aboutFrame;
-    private javax.swing.JLabel aboutLabel;
     private javax.swing.JLabel blueLabel;
     private javax.swing.JSlider blueSlider;
     private javax.swing.JLabel colorLabel;
@@ -414,8 +310,6 @@ public class ColorPicker extends javax.swing.JFrame {
     private javax.swing.JMenuItem menuBarFile_Exit;
     private javax.swing.JMenuItem menuBarFile_New;
     private javax.swing.JPopupMenu.Separator menuBarFile_Separator;
-    private javax.swing.JMenu menuBarHelp;
-    private javax.swing.JMenuItem menuBarHelp_About;
     private javax.swing.JPanel previewPanel;
     private javax.swing.JLabel redLabel;
     private javax.swing.JSlider redSlider;
